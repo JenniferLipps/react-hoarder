27ad732e13fe863d248a17b207889c366d9a107b
Jennifer Lipps <jjlipps@gmail.com>
2019-07-05 23:34:02 -0500

    working on home/auth/nav

---
+++ src/Home/Home.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Home extends React.Component {
+  render() {
+    return (
+      <div className="Home">
+        <h2>Home</h2>
+        <button className="btn btn-warning">Hoarder</button>
+      </div>
+    );
+  }
+}
+
+export default Home;
