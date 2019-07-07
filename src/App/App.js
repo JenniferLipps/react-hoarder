97ebdbadec0acd8737457c28cb1f2b78ee44ad63
Jennifer Lipps <jjlipps@gmail.com>
2019-07-07 14:15:19 -0500

    wip, restart

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,79 @@
 import React from 'react';
+import {
+  BrowserRouter,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+import firebase from 'firebase/app';
+
 import 'bootstrap/dist/css/bootstrap.min.css';
-import Home from '../Home/Home';
-import MyNavBar from '../MyNavBar/MyNavBar';
+import Auth from '../components/Auth/Auth';
+import Home from '../components/Home/Home';
+import MyNavBar from '../components/MyNavBar/MyNavBar';
+import fbConnection from '../helpers/data/connection';
+
 import './App.scss';
 
-function App() {
-  return (
-    <div className="App">
-       <MyNavBar />
-       <Home />
-    </div>
+fbConnection();
+
+const PublicRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === false
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/home', state: { from: props.location } }} />)
   );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+const PrivateRoute = ({ component: Component, authed, ...rest }) => {
+  const routeChecker = props => (authed === true
+    ? (<Component {...props} />)
+    : (<Redirect to={{ pathname: '/auth', state: { from: props.location } }} />)
+  );
+  return <Route {...rest} render={props => routeChecker(props)} />;
+};
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+        <BrowserRouter>
+          <React.Fragment>
+            <MyNavBar authed={authed} />
+            <div className="container">
+              <div className="row">
+                <Switch>
+                  <PublicRoute path='/auth' component={Auth} authed={authed} />
+                  <PrivateRoute path='/home' component={Home} authed={authed} />
+                  <Redirect from="*" to="/auth" />
+                </Switch>
+              </div>
+            </div>
+          </React.Fragment>
+        </BrowserRouter>
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './Home.scss';
 
 class Home extends React.Component {
   render() {
+    const singleLink = '/scat/12345';
     return (
       <div className="Home">
         <h2>Home</h2>
+        <button className="btn btn-warning" onClick={this.editEvent}>Edit</button>
+        <Link to={singleLink}>View Single</Link>
       </div>
     );
   }
